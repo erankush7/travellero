5dcef119e393d525ef20a96f9e8dd59bfe61c6ed
Ankush Sharma <erankush7@gmail.com>
2018-03-10 20:53:09 +0530

    First commit Project Setup

---
+++ Travellero/Travellero/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Travellero
+//
+//  Created by Ankush Sharma on 10/03/18.
+//  Copyright © 2018 Ankush. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
